7160549902ab4e152f91f5fd00ac46b8a67078ff
Ameer Arsala <ameer.arsala03@gmail.com>
2024-11-12 03:56:22 -0800

    plugins start

--- plugins/full-border.yazi/init.lua
+++ plugins/full-border.yazi/init.lua
@@ -19,12 +19,7 @@ local function setup(_, opts)
 
 			return v4(
 				"bar",
-				ui.Rect({
-					x = x,
-					y = math.max(0, y),
-					w = ya.clamp(0, self._area.w - x, 1),
-					h = math.min(1, self._area.h),
-				}),
+				ui.Rect { x = x, y = math.max(0, y), w = ya.clamp(0, self._area.w - x, 1), h = math.min(1, self._area.h) },
 				ui.Bar.TOP
 			):symbol(c)
 		end
