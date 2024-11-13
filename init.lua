243e8c6932dfd12bb7b3934f0ab43d5d13cc5eb3
Ameer Arsala <ameer.arsala03@gmail.com>
2024-11-12 21:51:08 -0800

    plugins

--- init.lua
+++ init.lua
@@ -1,7 +1,9 @@
 require("git"):setup()
 
+require("starship"):setup()
+
 -- TODO: only do this if it's not within neovim
-require("full-border"):setup({
-	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-	type = ui.Border.ROUNDED,
-})
+-- require("full-border"):setup({
+-- 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+-- 	type = ui.Border.PLAIN,
+-- })
